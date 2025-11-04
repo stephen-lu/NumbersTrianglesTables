f8c08a26799f6bac22373a128b1725375ff4ec9c
Stephen <stephen.lu20@gmail.com>
2025-11-04 10:46:40 -0500

    Finished number utilities

--- java/src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ java/src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -4,38 +4,66 @@ package io.zipcoder.microlabs.mastering_loops;
 public class NumberUtilities {
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i+=step) {
+            range += String.valueOf((int) Math.pow(i, exponent));
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i+=step) {
+            range += Integer.toString(i);
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i++) {
+            range += Integer.toString(i);
+        }
+        return range;
     }
 
     public static String getRange(int stop) {
-        return null;
+        String range = "";
+        for (int i = 0; i < stop; i++) {
+            range += Integer.toString(i);
+        }
+        return range;
     }
 
     /*
      * natural break
      */
 
-    public static boolean isNumberEven(int toTest) { return false; }
-    public static boolean isNumberOdd(int toTest) { return false; }
+    public static boolean isNumberEven(int toTest) { return toTest % 2 == 0; }
+    public static boolean isNumberOdd(int toTest) { return toTest % 2 == 1; }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i++) {
+            if (isNumberEven(i)) {
+                range += Integer.toString(i);
+            }
+        }
+        return range;
     }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String range = "";
+        for (int i = start; i < stop; i++) {
+            if (isNumberOdd(i)) {
+                range += Integer.toString(i);
+            }
+        }
+        return range;
     }
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 2);
     }
 
 }
--- java/src/test/java/io/zipcoder/microlabs/mastering_loops/NumberUtilitiesTest.java
+++ java/src/test/java/io/zipcoder/microlabs/mastering_loops/NumberUtilitiesTest.java
@@ -184,7 +184,7 @@ public class NumberUtilitiesTest {
     @Test
     public void testGetEvenNumbers() {
         // : Given
-        String expected = "5791113151719";
+        String expected = "681012141618";
         int start = 5;
         int stop = 20;
 
@@ -198,10 +198,9 @@ public class NumberUtilitiesTest {
     @Test
     public void testGetOddNumbers() {
         // : Given
-        String expected = "681012141618";
+        String expected = "5791113151719";
         int start = 5;
         int stop = 20;
-        int step = 5;
 
         // : When
         String actual = NumberUtilities.getOddNumbers(start, stop);
