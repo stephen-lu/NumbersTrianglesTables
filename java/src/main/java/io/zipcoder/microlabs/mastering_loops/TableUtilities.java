2296d1c96b16adb1d0d7b8a8ca53587500c3290c
Stephen <stephen.lu20@gmail.com>
2025-11-05 12:45:19 -0500

    Changed table method to use string format specifier

--- java/src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ java/src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,17 +3,9 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TableUtilities {
     public static String getMultiplicationTable(int tableSize) {
         String table = "";
-        int maxNumberLength = String.valueOf(tableSize * tableSize).length();
-        if (maxNumberLength < 3) {
-            maxNumberLength = 3;
-        }
-        for (int i = 0; i < tableSize; i++) {
-            for (int j = 0; j < tableSize; j++) {
-                String num = String.valueOf((i+1)*(j+1));
-                for (int len = 0 + (num.length()); len < maxNumberLength; len++) {
-                    table += " ";
-                }
-                table += String.valueOf((i+1)*(j+1)) + " |";
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                table += String.format("%3d |", i*j);
             }
             table += "\n";
         }
